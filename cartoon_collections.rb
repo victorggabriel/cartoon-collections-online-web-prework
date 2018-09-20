17e955eeb11a95373c3fe8138b2782fe5e3dc13b
Victor Gonzalez <mywalletislost@gmail.com>
2018-09-20 20:04:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,9 @@ end
 
 def summon_captain_planet(planeteer_calls)
 
-  planeteer_calls.collect { |x| x.upcase << "!" } 
+ planeteer_calls.collect do |element|
+    element.capitalize + "!"
+  end 
   
 end
 
